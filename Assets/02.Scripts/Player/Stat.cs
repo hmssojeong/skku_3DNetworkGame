54d144a7c05746a02415ed3336c62f6a1ea5ebda
hmssojeong <sojeong9638@gmail.com>
2026-02-23 15:30:40 +0900

    feat: player 스탯 스크립트 분리

---
+++ Assets/02.Scripts/Player/Stat.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Stat
+{
+    public float MoveSpeed;
+    public float JumpPower;
+    public float RotationSpeed;
+    public float AttackSpeed;
+    public float AttackCoolTime;
+}
