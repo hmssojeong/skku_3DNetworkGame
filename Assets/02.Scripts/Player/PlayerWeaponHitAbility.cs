45e9955326e9329ee930b62a5ff78e6c0b74e8d8
hmssojeong <sojeong9638@gmail.com>
2026-02-25 15:57:44 +0900

    chore: damageable 수정

--- Assets/02.Scripts/Player/PlayerWeaponHitAbility.cs
+++ Assets/02.Scripts/Player/PlayerWeaponHitAbility.cs
@@ -1,9 +1,10 @@
 using Photon.Pun;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class PlayerWeaponHitAbility : PlayerAbility
 {
-    private void OnTriggerEnter(Collider other)
+private void OnTriggerEnter(Collider other)
     {
         if (!_owner.PhotonView.IsMine) return;
 
@@ -11,17 +12,29 @@ public class PlayerWeaponHitAbility : PlayerAbility
 
         if (other.TryGetComponent<IDamageable>(out var damageable))
         {
-            //damageable.TakeDamage(_owner.Stat.Damage);
-            
-            // 포톤에서는 Room 안에서 플레이어마다 고유 식별자(ID)인 ActorNumber를 가지고 있다.
-            int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
-            // int actorNumber = _owner.PhotonView.Owner.ActorNumber;
-
-            // 상대방의 TakeDamage를 RPC로 호출한다.
             PlayerController otherPlayer = other.GetComponent<PlayerController>();
-            otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage), RpcTarget.All, _owner.Stat.Damage, _owner.transform.position);
+            int attackerActorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+
+            otherPlayer.PhotonView.RPC(
+                nameof(damageable.TakeDamage),
+                RpcTarget.All,
+                _owner.Stat.Damage,
+                _owner.transform.position,
+                attackerActorNumber  // 누락됐던 파라미터 추가
+            );
 
             _owner.GetAbility<PlayerWeaponColliderAbility>().DeactiveCollider();
         }
     }
 }
+
+
+/*//damageable.TakeDamage(_owner.Stat.Damage);
+
+// 포톤에서는 Room 안에서 플레이어마다 고유 식별자(ID)인 ActorNumber를 가지고 있다.
+int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+// int actorNumber = _owner.PhotonView.Owner.ActorNumber;
+
+// 상대방의 TakeDamage를 RPC로 호출한다.
+PlayerController otherPlayer = other.GetComponent<PlayerController>();
+otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage), RpcTarget.All, _owner.Stat.Damage, _owner.transform.position);*/
