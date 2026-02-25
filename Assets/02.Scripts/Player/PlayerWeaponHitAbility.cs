6eb288df93e18dd774f0dd604325dfa0bbcecf9f
hmssojeong <sojeong9638@gmail.com>
2026-02-25 17:25:09 +0900

    chore: scene name 변경

--- Assets/02.Scripts/Player/PlayerWeaponHitAbility.cs
+++ Assets/02.Scripts/Player/PlayerWeaponHitAbility.cs
@@ -15,26 +15,9 @@ private void OnTriggerEnter(Collider other)
             PlayerController otherPlayer = other.GetComponent<PlayerController>();
             int attackerActorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
 
-            otherPlayer.PhotonView.RPC(
-                nameof(damageable.TakeDamage),
-                RpcTarget.All,
-                _owner.Stat.Damage,
-                _owner.transform.position,
-                attackerActorNumber  // 누락됐던 파라미터 추가
-            );
+            otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage),RpcTarget.All,_owner.Stat.Damage,_owner.transform.position,attackerActorNumber);
 
             _owner.GetAbility<PlayerWeaponColliderAbility>().DeactiveCollider();
         }
     }
 }
-
-
-/*//damageable.TakeDamage(_owner.Stat.Damage);
-
-// 포톤에서는 Room 안에서 플레이어마다 고유 식별자(ID)인 ActorNumber를 가지고 있다.
-int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
-// int actorNumber = _owner.PhotonView.Owner.ActorNumber;
-
-// 상대방의 TakeDamage를 RPC로 호출한다.
-PlayerController otherPlayer = other.GetComponent<PlayerController>();
-otherPlayer.PhotonView.RPC(nameof(damageable.TakeDamage), RpcTarget.All, _owner.Stat.Damage, _owner.transform.position);*/
