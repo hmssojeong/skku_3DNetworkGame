baa45901cf4cce842dbbb61d7f566390fc091b5c
hmssojeong <sojeong9638@gmail.com>
2026-03-06 12:33:30 +0900

    chore: 방장플레이어에게 참여한 플레이어 보이지않는 현상 수정

--- Assets/02.Scripts/Core/GameScene.cs
+++ Assets/02.Scripts/Core/GameScene.cs
@@ -3,8 +3,31 @@ using UnityEngine;
 
 public class GameScene : MonoBehaviourPunCallbacks
 {
+
+    private bool _isPlayerGenerated = false;
+
     private void Start()
     {
+        if(PhotonNetwork.InRoom)
+        {
+            GenteratePlayer();
+        }
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        GenteratePlayer();
+
+    }
+
+
+    private void GenteratePlayer()
+    {
+        if (_isPlayerGenerated) return;
+
+        _isPlayerGenerated = true;
+
         GameObject player = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
         PhotonNetwork.Instantiate("Object", Vector3.zero, Quaternion.identity);
         Debug.Log($"[GameScene] Player Instantiate 완료 | player null? {player == null} | ViewID: {player?.GetComponent<PhotonView>()?.ViewID}");
